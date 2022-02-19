877da62f47ae3cb64cefecc2749a80eb14c1e1aa
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-02-19 18:05:03 +0600

    understanding let and const once again

---
+++ let-const.js
@@ -0,0 +1,21 @@
+let balance = 1240;
+balance = 100;
+
+const userName = "janpahki potas potas";
+const weTogagher = "ami" + userName;
+console.log(weTogagher);
+
+const numbers = [45, 23, 89, 60];
+
+// numbers = [99, 145, 3]; //Not allowed
+
+numbers.push(5555);
+number[1] = 333;
+
+for (let i = 0; i > numbers.length; i++) {
+    const number = numbers[i];
+    console.log(number);
+}
+
+const student = { roll: 101, name: "mofiz", job: "intern" };
+student.name = "Mofazzol";
